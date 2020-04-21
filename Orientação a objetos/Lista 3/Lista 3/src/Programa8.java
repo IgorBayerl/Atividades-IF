450b9c079906f04bcfd888770e5e37b5c34866e4
Igor Bayerl <bayerl.igor@gmail.com>
2020-04-21 10:12:10 -0300

    questão 8

--- Orientação a objetos/Lista 3/Lista 3/src/Programa8.java
+++ Orientação a objetos/Lista 3/Lista 3/src/Programa8.java
@@ -1,43 +1,27 @@
-package demo;
+import java.util.*;
 
-import com.mtsystems.coot.String8;
 
-import java.util.Scanner;
+public class Programa8 {
 
-public class DemoTranslation {
-	public static void main() {
-		String8 string = new String8(41);
-		byte[] contrario = new byte[41];
-		int total = 0, j;
-		boolean erro = false;
-
-		System.out.print("Digite a palavra (ate 40 caracteres): ");
-		string.copyFrom(STDIN_SCANNER.next());
-
-		total = string.length();
-		j = total;
-		total++;
-		j--;
-
-		for(int i = 0; i < total; i++) {
-			contrario[i] = string.get(j);
-			j--;
-		}
-
-		total--;
-
-		for(int i = 0; i < total; i++) {
-			if(string.get(i) != contrario[i]) {
-				erro = true;
-			}
-		}
-
-		if(erro) {
-			System.out.print("\nNao e palindromo.");
-		} else {
-			System.out.print("\nE palindromo.");
-		}
+	public static void main(String[] args) {
+		
+        Scanner entrada = new Scanner(System.in);
+        
+        String stringNormal;
+            
+        
+        System.out.println(" Digite uma palavra: ");
+        stringNormal = entrada.next();
+        
+        StringBuffer numero = new StringBuffer(stringNormal);
+      
+        
+        if(stringNormal.equals(numero.reverse().toString())) 
+      	System.out.printf("  %s é um palíndromo.", stringNormal);
+        else
+      	System.out.printf("  %s não é um palíndromo.", stringNormal);
+        
+  
 	}
 
-	public final static Scanner STDIN_SCANNER = new Scanner(System.in);
 }
