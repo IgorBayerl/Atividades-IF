ca5f493d65377c8a8dfab49f26ec6d53aab59a5a
Igor Bayerl <bayerl.igor@gmail.com>
2020-04-21 15:41:00 -0300

    questão 4 completa

--- Orientação a objetos/Lista 4/Lista 4/src/Programa5.java
+++ Orientação a objetos/Lista 4/Lista 4/src/Programa5.java
@@ -1,12 +1,22 @@
 import java.util.*;
 
+
 public class Programa5 {
 
 	public static void main(String[] args) {
 		
 		int[] numeros;
+		int[] numerosPositivos;
+		int[] numerosNegativos;
+		
+		int numeroP = 0;
+        int numeroI = 0;
         
         numeros = new int[10];
+        numerosPositivos = new int[numeros.length];
+        numerosNegativos = new int[numeros.length];
+        
+        
         
         int x;
         System.out.print("Voce ira digitar 10 numeros: ");
@@ -16,14 +26,37 @@ public class Programa5 {
     		x = sc.nextInt();
     		
     		numeros [i] = x;
-        }
-        System.out.println();
+    		
+    		if(x > 0) {
+    			numerosPositivos [numeroP] = x;
+    			numeroP ++;
+    		}else {
+    			numerosNegativos [numeroI] = x;
+    			numeroI++;
+    		}
+    		
+    		
+        }        
         
+        System.out.println("\n Numeros Digitados");
         for(int i=0; i<numeros.length; i++){
-    		System.out.println("posicao " + (i) + " = " +numeros[i] );
+    		System.out.print( " - " + numeros[i]);
     		
     	}
-        
+        System.out.println("\n Numeros Positivos");
+        for(int i=0; i<numerosPositivos.length; i++){
+        	if (numerosPositivos[i] != 0) {
+    		System.out.print( " - " + numerosPositivos[i]);
+        	}
+    		
+    	}
+        System.out.println("\n Numeros Impares");
+        for(int i=0; i<numerosNegativos.length; i++){
+        	if (numerosNegativos[i] != 0) {
+    		System.out.print( " - " + numerosNegativos[i]);
+        	}
+    		
+    	}
 		
 	}
 }
